91d7bb4e0e7d24b1ddce7a334a8daaffb638d8aa
Beknur <106554639+BEknUur@users.noreply.github.com>
2025-01-28 20:03:30 +0500

    bekzat crush

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+  const overlay = document.querySelector(".menu-overlay");
+
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+  overlay.classList.toggle("open");
 }
+
 
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 
